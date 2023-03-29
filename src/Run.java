1d27d958f59882929253df6303f596066db72276
Rudy <rudyetzel@gmail.com>
2023-03-28 20:00:18 -0400

    Commit 1
    Added loop where random vehicles are chosen to "interact" with one
    another. At the end of the interaction, their trust values are adjusted.
    
    Also added an average trust value method to car class

--- src/Run.java
+++ src/Run.java
@@ -13,12 +13,39 @@ public class Run {
 		for(int i =0; i < totalCar_Count; i++){
 			carList.add(randomGenerator());
 		}
+		
+		sortByTrustValue(carList);
+		System.out.println(carList);
+		
+		//Random car interactions that adjust their trust values
+		for(int i=0; i< totalCar_Count; i++) {
+			Car carA = carList.get(carPicker());
+			Car carB = carList.get(carPicker());
+			
+			while(carA == carB) {
+				carB = carList.get(carPicker());
+			}
+			
+			carA.adjustTrust(trustValuePicker());
+			carB.adjustTrust(trustValuePicker());
+		}
 
 		sortByTrustValue(carList);
 		System.out.println(carList);
 
 	}
 	
+	public static double trustValuePicker() {
+		Random randVal = new Random();
+		Double randomValue = randVal.nextInt(10) + 1.0;
+		return randomValue;
+	}
+	
+	public static int carPicker() {
+		Random randCar = new Random();
+		int randomCar = randCar.nextInt(totalCar_Count);
+		return randomCar;
+	}
 	
 	public static Car randomGenerator() {
 		Random rand = new Random();
